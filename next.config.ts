b9f6e880818c2ca9555d89e117a9e59eb8085f11
Your Name <your.email@example.com>
2025-04-21 11:03:02 -0400

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,8 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/my_portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my_portfolio/' : '',
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
     if (config.module && config.module.rules) {
